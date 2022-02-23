5d30a690049620fd4d3c2a458f09e59d8b35865a
SahilZala <zala4011@gmail.com>
2022-02-23 19:03:41 +0530

    My first commit

---
+++ src/main/java/com/mark2/mglow/controller/Welcome.java
@@ -0,0 +1,12 @@
+package com.mark2.mglow.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "Welcome to our small page";
+	}
+}
